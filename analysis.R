d797b172ffae90d009c86c6ee07a54fc027b3143
Em T <etallman@uw.edu>
2022-05-22 13:33:36 -0700

    >:(

--- analysis.R
+++ analysis.R
@@ -1,8 +1,21 @@
 library(dplyr)
+library(tidyr)
 
 inc_trends <- read.csv("incarceration_trends.csv")
 
-no_na <- na.omit(inc_trends)
+#Checking which cols have missing data/when-------------------------
+
+na_count <- function(the_year){
+  total <- inc_trends %>%
+    filter(year == the_year) %>%
+    sapply(function(y) sum(length(which(is.na(y))))) %>%
+    data.frame() %>%
+    View()
+}
+
+na_count(2018)
+
+#Handy info----------------------------------------------------------
 
 region_summary <- inc_trends %>%
   group_by(region) %>%
@@ -13,37 +26,81 @@ state_reg <- select(inc_trends, state, region) %>%
   summarise(region) %>%
   distinct() 
 
-region_distribution_no_na <- no_na %>%
-  group_by(region) %>%
-  summarise(occurrences = n())
+#Cleaning--------------------------------------------------------------
 
-original_region_distribution <- inc_trends %>%
-  group_by(region) %>%
-  summarise(occurrences = n())
 
-year_summary <- inc_trends %>%
+filtered_1990_to_2018 <- inc_trends %>%
+  filter(year == 1990 | year == 1991 | year == 1992 | 
+           year == 1993 | year == 1994 | year == 1995 | year == 1996 | 
+           year == 1997 | year == 1998 | year == 1999 | year == 2000 | 
+           year == 2001 | year == 2002 | year == 2003 | year == 2004 | year == 2005 |
+           year == 2006 | year == 2007 | year == 2008 | year == 2009  | year == 2010 | 
+           year == 2011 | year == 2012 | year == 2013 | year == 2014 | year == 2015 | 
+           year == 2016 | year == 2017 | year == 2018) %>%
+  select(year, state, county_name, region, urbanicity, division, total_pop_15to64,
+         aapi_pop_15to64, black_pop_15to64, latinx_pop_15to64, 
+         native_pop_15to64, white_pop_15to64
+  ) 
+  
+year_avg <- filtered_1990_to_2018 %>%
   group_by(year) %>%
-  summarise(occurrences = n())
+  summarise(total_mean = mean(total_pop_15to64), aapi_mean = mean(aapi_pop_15to64), 
+            black_mean = mean(black_pop_15to64), latino_mean = mean(latinx_pop_15to64), 
+            native_mean = mean(native_pop_15to64), white_mean = mean(white_pop_15to64))
+
+year_avg_simplified <- year_avg %>%
+  select(-white_mean, -year, -total_mean)  
+year_avg_simplified <- mutate(year_avg_simplified, non_white_mean_total = 
+                                rowSums(year_avg_simplified)) %>%
+  select(non_white_mean_total) 
+year_avg_simplified <-  merge(year_avg, year_avg_simplified)
 
 
-inc_trends$na_count <- rowSums(
-  is.na(inc_trends)
-)
 
-na_by_year <- aggregate(
-  inc_trends$na_count,
-  by = list(year = inc_trends$year),
-  FUN = sum
-)
+  gather(
+    key = "race",
+    value = "population",
+    -year
+  ) 
+  
 
-inc_2016 <- inc_trends %>%
-  filter(year == 2016)
+state_avg <- filtered_1990_to_2018 %>%
+  group_by(state) %>%
+  summarise(total_mean = mean(total_pop_15to64), aapi_mean = mean(aapi_pop_15to64), 
+            black_mean = mean(black_pop_15to64), latino_mean = mean(latinx_pop_15to64), 
+            native_mean = mean(native_pop_15to64), white_mean = mean(white_pop_15to64))
 
-inc_2006 <- inc_trends %>%
-  filter(year == 2006)
+region_avg <- filtered_1990_to_2018 %>%
+  group_by(region) %>%
+  summarise(total_mean = mean(total_pop_15to64), aapi_mean = mean(aapi_pop_15to64), 
+            black_mean = mean(black_pop_15to64), latino_mean = mean(latinx_pop_15to64), 
+            native_mean = mean(native_pop_15to64), white_mean = mean(white_pop_15to64))
 
-inc_2011 <- inc_trends %>%
-  filter(year == 2011)
+
+
+avg_sum_year <- combined_dfs %>%    
+  group_by(year) %>%
+  summarise(white = mean(white_prison_pop_rate), non_white = mean(non_white_pop_rate_total))%>%
+  round()
+
+
+gathered <- avg_sum_year %>%
+  gather(
+    key = "race",
+    value = "population_rate",
+    -year
+  )
+
+gathered2 <- combined_dfs %>%
+  group_by(region) %>%
+  summarise(white = mean(white_prison_pop_rate), non_white = mean(non_white_pop_rate_total), all_population = mean(total_prison_pop_rate)) %>%
+  gather(
+    key = "race",
+    value = "population_rate",
+    -region
+  )
+
+
 
 
 race_2006_state_pop_rate <- inc_2006 %>%
@@ -161,23 +218,7 @@ combined_dfs <- rbind(a2006_df, a2011_df) %>%
 library(rworldmap) 
 library(RColorBrewer)
 
-na_count <- function(the_year){
-search <- inc_trends %>%
-  filter(year == the_year) %>%
-  select(state, total_prison_pop_rate,
-         aapi_prison_pop_rate,
-         black_prison_pop_rate,latinx_prison_pop_rate,
-         native_prison_pop_rate,white_prison_pop_rate,) %>%
-  group_by(state) %>%
-  summarise(total = mean(total_prison_pop_rate), aapi = mean(aapi_prison_pop_rate), 
-            black = mean(black_prison_pop_rate), latino = mean(latinx_prison_pop_rate), 
-            white = mean(white_prison_pop_rate))
- rowSums(is.na(search)) %>%
-   sum() %>%
-  print()
-}
 
-na_count(2006)
 
 
 
