f03e61b897154617f0632cf14a2e8cb75b1c1b8f
Em T <etallman@uw.edu>
2022-05-21 17:05:19 -0700

    qs

---
+++ analysis.R
@@ -0,0 +1,120 @@
+library(dplyr)
+
+inc_trends <- read.csv("incarceration_trends.csv")
+
+no_na <- na.omit(inc_trends)
+
+region_summary <- inc_trends %>%
+  group_by(region) %>%
+  summarise()
+
+state_reg <- select(inc_trends, state, region) %>%
+  group_by(state) %>%
+  summarise(region) %>%
+  distinct() 
+
+region_distribution_no_na <- no_na %>%
+  group_by(region) %>%
+  summarise(occurrences = n())
+
+original_region_distribution <- inc_trends %>%
+  group_by(region) %>%
+  summarise(occurrences = n())
+
+year_summary <- inc_trends %>%
+  group_by(year) %>%
+  summarise(occurrences = n())
+
+
+inc_trends$na_count <- rowSums(
+  is.na(inc_trends)
+)
+
+na_by_year <- aggregate(
+  inc_trends$na_count,
+  by = list(year = inc_trends$year),
+  FUN = sum
+)
+
+inc_2016 <- inc_trends %>%
+  filter(year == 2016)
+
+inc_2006 <- inc_trends %>%
+  filter(year == 2006)
+
+inc_2011 <- inc_trends %>%
+  filter(year == 2011)
+
+race_2006_state_pop_rate <- inc_2006 %>%
+  select(state, total_prison_pop_rate,
+    aapi_prison_pop_rate,
+         black_prison_pop_rate,latinx_prison_pop_rate,
+         native_prison_pop_rate,white_prison_pop_rate,)
+race_2006_state_pop_rate <- aggregate(
+  . ~ state, race_2006_state_pop_rate, sum
+) 
+race_2006_state_pop_rate <- merge(
+  race_2006_state_pop_rate, state_reg, all.x = T
+)
+
+race_2011_state_pop_rate <- inc_2011 %>%
+  select(state, total_prison_pop_rate,
+         aapi_prison_pop_rate,
+         black_prison_pop_rate,latinx_prison_pop_rate,
+         native_prison_pop_rate,white_prison_pop_rate,)
+race_2011_state_pop_rate <- aggregate(
+  . ~ state, race_2011_state_pop_rate, sum
+) 
+race_2011_state_pop_rate <- merge(
+  race_2011_state_pop_rate, state_reg, all.x = T
+)
+
+race_2016_state_pop_rate <- inc_2016 %>%
+  select(state, total_prison_pop_rate,
+         aapi_prison_pop_rate,
+         black_prison_pop_rate,latinx_prison_pop_rate,
+         native_prison_pop_rate,white_prison_pop_rate,)
+race_2016_state_pop_rate <- aggregate(
+  . ~ state, race_2016_state_pop_rate, sum
+) 
+race_2016_state_pop_rate <- merge(
+  race_2016_state_pop_rate, state_reg, all.x = T
+)
+
+race_2006_REGION_pop_rate <- race_2006_state_pop_rate %>%
+  subset(select = -c(state)) 
+race_2006_REGION_pop_rate <- aggregate(
+  . ~ region, race_2006_REGION_pop_rate, sum
+  ) %>%
+  mutate(non_white_pop_rate_total = aapi_prison_pop_rate + black_prison_pop_rate + 
+           latinx_prison_pop_rate + native_prison_pop_rate) %>%
+  mutate(non_white_pop_rate_DIFF = 
+    non_white_pop_rate_total - total_prison_pop_rate) %>%
+  mutate(white_pop_rate_DIFF = 
+    white_prison_pop_rate - total_prison_pop_rate) %>%
+  mutate_if(is.numeric, round)
+
+  
+#ratio version
+  #mutate_if(is.numeric, round) %>%
+  #mutate(non_white_pop_rate_RATIO = round(
+   # non_white_pop_rate_total / total_prison_pop_rate, digits = 2)) %>%
+  #mutate(white_pop_rate_RATIO = round(
+   # white_prison_pop_rate / total_prison_pop_rate, digits = 2)) 
+
+race_2011_REGION_pop_rate <- race_2011_state_pop_rate %>%
+  subset(select = -c(state)) 
+race_2011_REGION_pop_rate <- aggregate(
+  . ~ region, race_2011_REGION_pop_rate, sum
+) %>%
+  mutate_if(is.numeric, round)
+
+race_2016_REGION_pop_rate <- race_2016_state_pop_rate %>%
+  subset(select = -c(state)) 
+race_2016_REGION_pop_rate <- aggregate(
+  . ~ region, race_2016_REGION_pop_rate, sum
+) %>%
+  mutate_if(is.numeric, round)
+
+
+
