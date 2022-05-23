3649911c37e2b7955b49587b0c1bf0da39f7c133
Em T <etallman@uw.edu>
2022-05-22 19:31:43 -0700

    i think i'm done

--- analysis.R
+++ analysis.R
@@ -66,6 +66,19 @@ year_avg_simplified <-  cbind(year_avg, year_avg_simplified) %>%
     value = "mean_rate",
     -year
   ) 
+year_avg_simplified$race <- str_replace_all(year_avg_simplified$race, "white", "White")
+year_avg_simplified$race <- str_replace_all(year_avg_simplified$race, "non_White", "Non-White")
+year_avg_simplified$race <- str_replace_all(year_avg_simplified$race, 
+                                            "total_population", "Total Population")
+
+year_avg_compared <- year_avg %>%
+  select(-white_mean, -year, -total_mean)  
+year_avg_compared <- mutate(year_avg_compared, non_white_mean_total = rowSums(year_avg_compared)) %>%
+  select(non_white_mean_total) 
+year_avg_compared <-  cbind(year_avg, year_avg_compared) %>%
+  select(year, white_mean, non_white_mean_total) %>%
+  mutate(diff = non_white_mean_total - white_mean) %>%
+  arrange(desc(diff))
   
 
 state_avg <- filtered_1998_to_2018 %>%
@@ -120,6 +133,15 @@ region_2018_avg <- filtered_1998_to_2018 %>%
             native_mean = mean(native_jail_pop_rate, na.rm = TRUE), 
             white_mean = mean(white_jail_pop_rate, na.rm = TRUE))
 
+region_avg_compared_2018 <- region_2018_avg %>%
+  select(-white_mean, -region, -total_mean) 
+region_avg_compared_2018 <- mutate(region_avg_compared_2018, non_white_mean_total = 
+                                       rowSums(region_avg_compared_2018)) %>%
+  select(non_white_mean_total) 
+region_avg_compared_2018 <-  cbind(region_2018_avg, region_avg_compared_2018) %>%
+  mutate(diff = non_white_mean_total - white_mean) %>%
+  arrange(desc(diff))
+
 region_2018_avg_simplified <- region_2018_avg %>%
 select(-white_mean, -region, -total_mean) 
 region_2018_avg_simplified <- mutate(region_2018_avg_simplified, non_white_mean_total = 
@@ -134,5 +156,20 @@ region_2018_avg_simplified <-  cbind(region_2018_avg, region_2018_avg_simplified
     -region
   ) 
 
-race_only_region_2018 <- slice(region_2018_avg_simplified, 5:12)
+race_only_region_2018 <- slice(region_2018_avg_simplified, 5:12) #%>%
+race_only_region_2018$race <- str_replace_all(race_only_region_2018$race, "white", "White")
+race_only_region_2018$race <- str_replace_all(race_only_region_2018$race, "non_White", "Non-White")
 
+#Values----------------------------------------------------------------------
+
+year_highest_diff <- year_avg_compared$year[1]
+mean_diff <- round(summarise(year_avg_compared, mean(diff)))
+mean_diff <- format(mean_diff, big.mark = ',')
+highest_diff <- round(year_avg_compared$diff[1]) 
+highest_diff <- format(highest_diff, scientific  = FALSE, big.mark = ',')
+region_highest_rate_diff <- region_avg_compared_2018$region[1]
+state_rate_rank <- state_avg_simplified %>%
+  arrange(desc(non_white_mean_total))
+state_highest_rate_overall <- state_rate_rank$state[1]
+highest_rate <- format(round(state_rate_rank$non_white_mean_total[1]), scientific  = FALSE, big.mark = ',')
+mean_rate <- format(round(summarise(state_rate_rank, mean(non_white_mean_total))), scientific  = FALSE, big.mark = ',')
